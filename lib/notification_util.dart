4dcdcf67005649c21f69698132ffff3d061c0e18
yonggipo <yonggipo@icloud.com>
2024-09-12 17:51:32 +0900

    fix: fix build.gradle error

--- lib/notification_util.dart
+++ lib/notification_util.dart
@@ -1,16 +1,29 @@
+import 'dart:async';
 import 'dart:developer' as dev;
+import 'package:notification_listener_service/notification_listener_service.dart';
+
 import 'sampling_interval.dart';
 import 'sensor_util.dart';
 
 final class NotificationUtil implements SensorUtil {
+  static final NotificationUtil shared = NotificationUtil._();
+  NotificationUtil._();
+  factory NotificationUtil() => shared;
+
+  StreamSubscription? _subscription;
+
+  @override
+  SamplingInterval samplingInterval = SamplingInterval.event;
+
   @override
   void cancel() {
-    // TODO: implement cancel
+    _subscription?.cancel();
+    _subscription = null;
   }
 
   @override
   void onData(object) {
-    // TODO: implement onData
+    dev.log('<notification util> notification: $object');
   }
 
   @override
@@ -18,17 +31,20 @@ final class NotificationUtil implements SensorUtil {
     // TODO: implement onError
   }
 
+  /// request notification permission
   @override
-  Future<bool> requestPermission() {
-    // TODO: implement requestPermission
-    throw UnimplementedError();
+  Future<bool> requestPermission() async {
+    return NotificationListenerService.requestPermission();
   }
 
   @override
-  SamplingInterval samplingInterval = SamplingInterval.event;
-
-  @override
-  void start() {
-    // TODO: implement start
+  void start() async {
+    bool status = await NotificationListenerService.isPermissionGranted();
+    if (!status) {
+      if (await requestPermission()) {
+        /// stream the incoming notification events
+        NotificationListenerService.notificationsStream.listen(onData);
+      }
+    }
   }
 }
